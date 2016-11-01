7ffe6414fda118a30d1b5fd2e26463de0600df16
vilhelmfalkenmark <vilhelmfalkenmark@gmail.com>
2016-11-01 09:51:13 +0100

    KOMBI-122: First draft of login-form done

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,13 +1,18 @@
-$(document).ready(function() {
+$(".Login-toggle-help--js").click(function() {
+  $(this).next().slideToggle();
+});
+
+
+$(".Login-clickarea--js, .Login-close-form--js").click(function() {
+  $(".Login-overlay-container").fadeOut("fast");
+});
+
 $(".Menu-row-menu--js").click(function() {
-$(".Menu-row-burger").toggleClass("Burger--open")
-
-
-
+  $(".Menu-row-burger").toggleClass("Burger--open")
 })
 
-
-
+$(".Menu-row-profile--js, .Correct-lottery-btn--js").click(function() {
+    $(".Login-overlay-container").fadeIn("fast");
 })
 
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIk1lbnUtcm93LmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsImZpbGUiOiJtYWluLmpzIiwic291cmNlc0NvbnRlbnQiOlsiJChkb2N1bWVudCkucmVhZHkoZnVuY3Rpb24oKSB7XG4kKFwiLk1lbnUtcm93LW1lbnUtLWpzXCIpLmNsaWNrKGZ1bmN0aW9uKCkge1xuJChcIi5NZW51LXJvdy1idXJnZXJcIikudG9nZ2xlQ2xhc3MoXCJCdXJnZXItLW9wZW5cIilcblxuXG5cbn0pXG5cblxuXG59KVxuIl19
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIkxvZ2luLmpzIiwiTWVudS1yb3cuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FDUkE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsImZpbGUiOiJtYWluLmpzIiwic291cmNlc0NvbnRlbnQiOlsiJChcIi5Mb2dpbi10b2dnbGUtaGVscC0tanNcIikuY2xpY2soZnVuY3Rpb24oKSB7XG4gICQodGhpcykubmV4dCgpLnNsaWRlVG9nZ2xlKCk7XG59KTtcblxuXG4kKFwiLkxvZ2luLWNsaWNrYXJlYS0tanMsIC5Mb2dpbi1jbG9zZS1mb3JtLS1qc1wiKS5jbGljayhmdW5jdGlvbigpIHtcbiAgJChcIi5Mb2dpbi1vdmVybGF5LWNvbnRhaW5lclwiKS5mYWRlT3V0KFwiZmFzdFwiKTtcbn0pO1xuIiwiJChcIi5NZW51LXJvdy1tZW51LS1qc1wiKS5jbGljayhmdW5jdGlvbigpIHtcbiAgJChcIi5NZW51LXJvdy1idXJnZXJcIikudG9nZ2xlQ2xhc3MoXCJCdXJnZXItLW9wZW5cIilcbn0pXG5cbiQoXCIuTWVudS1yb3ctcHJvZmlsZS0tanMsIC5Db3JyZWN0LWxvdHRlcnktYnRuLS1qc1wiKS5jbGljayhmdW5jdGlvbigpIHtcbiAgICAkKFwiLkxvZ2luLW92ZXJsYXktY29udGFpbmVyXCIpLmZhZGVJbihcImZhc3RcIik7XG59KVxuIl19
