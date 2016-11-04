a72b72f6f0d85c9abfe77279f7dae902cb9af412
vilhelmfalkenmark <vilhelmfalkenmark@gmail.com>
2016-11-04 13:29:38 +0100

    First rebuild

--- dev/templates/components/lottery/Lottery.js
+++ dev/templates/components/lottery/Lottery.js
@@ -3,90 +3,56 @@
  /////////////////////////////////////////////
 $(document).ready(function() {
 // The customers lotteryticket
-
-var userNumbers = [9,5,2,5,3]
-// All the correct numbers
-
-var winningNumbers = {
-first: [
-  [6,5,4,5,7]
-],
-second: [
-  [4,5,2,8,4],
-  [6,5,4,5,7],
-  [9,3,8,8,6],
-  [1,5,3,3,4]
-],
-third: [
-  [3,4,5,3,4],
-  [1,3,4,9,2],
-  [3,3,6,7,8],
-  [0,5,2,8,4],
-  [6,5,4,5,7],
-  [3,3,6,8,6],
-  [3,3,8,7,4]
-]
-}
+var numbers = [6,5,4,5,6,3]
+var counter = 1;
 
 var correctNumbers = [
-  [3,4,5,3,4], // 1 rätt
-  [1,3,4,9,2], // 1 rätt
-  [3,3,6,7,8], // Alla rätt
-  [0,5,2,8,4], // Alla Fel
-  [1,1,6,5,3], // Alla Fel
-  [3,3,6,8,6], // 0, 1, 2 rätt
-  [3,3,8,7,4]  // 0, 1, 3 rätt
+  {
+    header: "1 000 000",
+    numbers: [
+      [6,5,4,5,6,1]
+    ]
+  },
+  {
+    header: "300 000",
+    numbers: [
+      [3,4,5,3,4,2],
+      [6,5,7,5,6,3],
+      [6,5,4,5,6,8]
+    ]
+  },
+  {
+    header: "100 000",
+    numbers: [
+      [3,4,5,3,4,5],
+      [1,3,4,9,2,3],
+      [3,3,6,7,8,2],
+      [0,5,2,8,4,9],
+    ]
+  }
 ]
+
 // Check how correct each array is in number. Eg
 // If an array matches by two digits the value will be two
 var correctAmount = [];
 var winning = false;
 
-var correctAmount2 = {
-  first: [],
-  second: [],
-  third: []
-};
-
-for (var key in winningNumbers) {
-
-  for (var i = 0; i < winningNumbers[key].length; i++) {
-    let correct = 0;
-    // console.log(winningNumbers[key][i]);
-    for (var  j= 0; j < winningNumbers[key][i].length; j++) {
-      // console.log(winningNumbers[key][i][j]);
-      if(winningNumbers[key][i][j] === userNumbers[j])
-      {
-          correct++;
-      }
-    }
-    correctAmount2[key].push(correct);
-  }
-}
-
-console.log(correctAmount2);
-
-
-
-
 // Function to push the value to the array
-checkArray = (x) => {
+checkArray = (winningNumbers) => {
   let correct = 0;
-  for (var i = 0; i < correctNumbers[x].length; i++) {
-      if(correctNumbers[x][i] === userNumbers[i]) {
-      correct++;
-      }
-    }
-    correctAmount.push(correct);
+  for (var i = 0; i < winningNumbers.length; i++) {
+        if(winningNumbers[i] === numbers[i]) {
+        correct++;
+        }
+  }
+  correctAmount.push(correct);
 }
-
 
 
 // Call the function
-for (var i = 0; i < correctNumbers.length; i++) {
-checkArray(i);
+for (var i = 0; i < correctNumbers[counter].numbers.length; i++) {
+  checkArray(correctNumbers[counter].numbers[i])
 }
-// console.log(correctAmount);
 // console.log(correctAmount);
 
 
@@ -95,16 +61,17 @@ getMaxOfArray = (numArray) =>  Math.max.apply(null, numArray);
 
 // Get the number
 var highestNumber = getMaxOfArray(correctAmount);
-if(highestNumber === userNumbers.length) {
+
+// console.log(highestNumber);
+
+if(highestNumber === numbers.length) {
   winning = true;
 }
-// The array that we are gonna use in the animation and compare with
-var drawArray = correctNumbers[correctAmount.indexOf(highestNumber)];
+// console.log(winning);
 
 
-// console.log(drawArray);
-
-// console.log(drawArray+" är den närmaste och den vi kommer utgå från");
+// The array that the drawing will be based on
+var drawArray = correctNumbers[counter].numbers[correctAmount.indexOf(highestNumber)];
 
 /////////////////////////////////////////////
 // *
@@ -115,10 +82,10 @@ var lotteryNumbers = document.getElementsByClassName('Lottery-number');
 
 var spinnInterval = 200; // Interval for spinn start for each number
 
-for (var i = 0; i < userNumbers.length; i++) {
+for (var i = 0; i < numbers.length; i++) {
   for (var j = 0; j < 10; j++) {
   var number = document.createElement("div");
-  if(j == userNumbers[i]) {
+  if(j == numbers[i]) {
   number.innerHTML = j;
   // number.className = "";
   } else {
@@ -160,30 +127,30 @@ if(startInterval) {
 
 var y = 0;
 var spinnTime = 2500; // HOW LONG DOES THE ANIMATION, SYNC WITH CSS FILE
-var ratio = spinnTime * ( userNumbers[0] / 10);
+var ratio = spinnTime * ( numbers[0] / 10);
 
 function correct() {
 
   var stop = 4000; // INITIAL VALUE (Kinda pointless)
   var diff = 0;
 
-  if(y < userNumbers.length)
+  if(y < numbers.length)
     {
-      if(userNumbers[y+1] > userNumbers[y]) {
-        diff = userNumbers[y+1] - userNumbers[y];
+      if(numbers[y+1] > numbers[y]) {
+        diff = numbers[y+1] - numbers[y];
       }
       else {
-        diff = 10 - (userNumbers[y] - userNumbers[y+1]);
+        diff = 10 - (numbers[y] - numbers[y+1]);
       }
 
       startInterval ? stop = spinnInterval + (( diff * spinnTime )/10) : stop =  (( diff * spinnTime )/10);
 
       lotteryNumbers[y].className = "Lottery-number";
 
-      if(userNumbers[y] === drawArray[y]) {
-        lotteryNumbers[y].innerHTML = "<div class='Lottery-correct'>"+userNumbers[y]+"</div>";
+      if(numbers[y] === drawArray[y]) {
+        lotteryNumbers[y].innerHTML = "<div class='Lottery-correct'>"+numbers[y]+"</div>";
       } else {
-        lotteryNumbers[y].innerHTML = "<div class='Lottery-wrong'>"+userNumbers[y]+"</div>";
+        lotteryNumbers[y].innerHTML = "<div class='Lottery-wrong'>"+numbers[y]+"</div>";
       }
 
       setTimeout(function()
--- gulpfile.js
+++ gulpfile.js
@@ -70,7 +70,7 @@ gulp.task('nunjucks', function() {
 /////////////////////////////////////////////
 gulp.task('js', function() {
 	return gulp.src([
- // 'dev/templates/components/lottery/Lottery.js',
+ 'dev/templates/components/lottery/Lottery.js',
  'dev/templates/components/login/Login.js',
  'dev/templates/components/menu/Menu.js',
  'dev/templates/components/menu-row/Menu-row.js'
