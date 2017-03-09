92d369cd04bb0ef1bb6e47dda3d5dc22e36002b5
vilhelmfalkenmark <vilhelmfalkenmark@gmail.com>
2017-03-09 15:31:04 +0100

    fixed scroll stuff and email regex

--- dev/templates/components/Submenu/Submenu.js
+++ dev/templates/components/Submenu/Submenu.js
@@ -1,7 +1,12 @@
- if ($.contains(document, $("#SubmenuAnchor")[0]) && ($(window).width() < 768)) {
-     setTimeout(function() {
-       $('html, body').animate({
-         scrollTop: $("#SubmenuAnchor").offset().top
-       }, 500);
-     },300)
+function mobileScroll(distance,delay) {
+    setTimeout(function(){
+      $('html, body').animate({
+        scrollTop: distance
+      }, 400);
+ }, delay);
+}
+ if ($.contains(document, $("#SubmenuAnchor")[0]) && $.contains(document, $("#anchor")[0]) === false && ($(window).width() < 768)) {
+   mobileScroll($("#SubmenuAnchor").offset().top,800)
+ } else if ($.contains(document, $("#SubmenuAnchor")[0]) && $.contains(document, $("#anchor")[0]) && ($(window).width() < 768)) {
+   mobileScroll($("#SubmenuAnchor").offset().top + $("#SubmenuAnchor").height(),800)
  }
